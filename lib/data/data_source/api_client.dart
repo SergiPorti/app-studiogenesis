ff7105d8aaf5429b74998d6524e536c142e813ac
SergiPorti <serporterolo01@gmail.com>
2023-09-07 04:46:26 +0200

    Created MultiPartRequest method + environments

--- lib/data/data_source/api_client.dart
+++ lib/data/data_source/api_client.dart
@@ -9,10 +9,9 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient {
-  Future<Either<Failure, Map<String, dynamic>>> get(String path,
-      {Function(int)? onError}) async {
+  Future<Either<Failure, Map<String, dynamic>>> get(String path) async {
     _verifyPathFormat(path);
-    final url = "${Environment.host}$path";
+    final url = "${Environment.ngrokApiHostUrl}$path";
 
     debugPrint(url);
 
@@ -33,10 +32,10 @@ class ApiClient {
     return Left(_parseError(res.statusCode, res.body));
   }
 
-  Future<Either<Failure, Map<String, dynamic>>> post(String path, Object data,
-      {Function(int)? onError}) async {
+  Future<Either<Failure, Map<String, dynamic>>> post(
+      String path, Object data) async {
     _verifyPathFormat(path);
-    final url = "${Environment.host}$path";
+    final url = "${Environment.ngrokApiHostUrl}$path";
     debugPrint(url);
 
     final res = await http.post(Uri.parse(url),
@@ -55,10 +54,10 @@ class ApiClient {
     return Left(_parseError(res.statusCode, res.body));
   }
 
-  Future<Either<Failure, Map<String, dynamic>>> delete(String path, Object data,
-      {Function(int)? onError}) async {
+  Future<Either<Failure, Map<String, dynamic>>> delete(
+      String path, Object data) async {
     _verifyPathFormat(path);
-    final url = "${Environment.host}$path";
+    final url = "${Environment.ngrokApiHostUrl}$path";
     debugPrint(url);
 
     final res = await http.delete(Uri.parse(url),
@@ -76,7 +75,41 @@ class ApiClient {
     debugPrint("${res.statusCode} message: ${res.body}");
     return Left(_parseError(res.statusCode, res.body));
   }
-  
+
+  Future<Either<Failure, Map<String, dynamic>>> multiPart(String path,
+      {required Map<String, String> data,
+      required List<String> filesPath,
+      required String typeOfFile}) async {
+    _verifyPathFormat(path);
+    final url = "${Environment.ngrokApiHostUrl}$path";
+    debugPrint(url);
+
+    final request = http.MultipartRequest('POST', Uri.parse(url))
+      ..fields.addAll(data);
+
+    request.headers.addAll(await _buildHeaders(isMultipartRequest: true));
+
+    for (final filePath in filesPath) {
+      final file = await http.MultipartFile.fromPath(typeOfFile, filePath);
+      request.files.add(file);
+    }
+
+    final res = await request.send();
+
+    final responseData = await res.stream.bytesToString();
+    debugPrint("StatusCode: ${res.statusCode}, body: $responseData");
+
+    if (res.statusCode == 200) {
+      try {
+        return Right(_parseResponse(responseData));
+      } catch ($e) {
+        return Left(Failure(message: $e.toString()));
+      }
+    }
+    debugPrint("${res.statusCode} message: $responseData");
+    return Left(_parseError(res.statusCode, responseData));
+  }
+
   Map<String, dynamic> _parseResponse(String body) {
     if (body.isNotEmpty) {
       Map<String, dynamic> response = jsonDecode(body);
@@ -86,13 +119,16 @@ class ApiClient {
     }
   }
 
-
-  Future<Map<String, String>> _buildHeaders() async {
+  Future<Map<String, String>> _buildHeaders(
+      {bool isMultipartRequest = false}) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString("token") ?? "";
 
     final Map<String, String> map = {
-      "Content-Type": "application/json",
+      "Content-Type": isMultipartRequest
+          ? "application/json; boundary=----WebKitFormBoundaryyEmKNDsBKjB7QEqu"
+          : "application/json",
+      "Host": Environment.ngrokHost
     };
 
     if (token.isNotEmpty) {
--- lib/environment.dart
+++ lib/environment.dart
@@ -1,3 +1,6 @@
 class Environment {
   static String host = "http://10.0.2.2:8000/api/";
+  static String ngrokHost = "https://4d29-213-147-130-141.ngrok-free.app";
+  static String ngrokApiHostUrl = "$ngrokHost/api/";
+  static String ngrokImageUrl = "$ngrokHost/public/";
 }
